0e3773db629ef65a8608aedce995f8c779c3010d
ctrl3352 <myloveyfair@hotmail.com>
2016-07-19 17:03:12 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -3,15 +3,15 @@ $(function(){
     var duration = 300;
 
     // images ----------------------------------------
-    var $images = $('#images p');
+    var $images = $('#works p');
 
     // images 第一個圖片
     $images.filter(':nth-child(1)')
         .on('mouseover', function(){
-            $(this).find('strong, span').stop(true).animate({opacity: 1}, duration);
+            $(this).find('span, strong, button').stop(true).animate({opacity: 1}, duration);
         })
         .on('mouseout', function(){
-            $(this).find('strong, span').stop(true).animate({opacity: 0}, duration);
+            $(this).find('span, strong, button').stop(true).animate({opacity: 0}, duration);
         });
 
 });
